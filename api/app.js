ab3d1f4e339349a41488dd380a42d21f0ce9044f
chavimiller <chaviweisman@gmail.com>
2025-12-30 15:31:42 +0200

    set up router files structure

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/authRouter");
+const postRouter = require("./routes/postRouter");
+const commentRouter = require("./routes/commentRouter");
 
 app.get("/", (req, res) => res.send("Homepage"));
 
 app.use("/auth", authRouter);
-app.use("/post", postRouter);
-app.use("/comment", commentRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
