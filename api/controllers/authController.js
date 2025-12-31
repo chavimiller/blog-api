f60dd866a2f0faae631b7cf94cf3d6ed57b98bfe
chavimiller <chaviweisman@gmail.com>
2025-12-31 10:20:33 +0200

    add path for login and signup routes

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,8 +1,9 @@
-const prisma = require("../prisma");
+// const prisma = require("../prisma");
 
 // get signup
 async function signUpGet(req, res) {
   try {
+    res.render("signup", { errors: [], data: [] });
   } catch (err) {
     console.error("ERROR with signUpGet: " + err);
     res.status(500).send("Server error");
@@ -12,9 +13,17 @@ async function signUpGet(req, res) {
 // post signup
 
 // get login
+async function loginGet(req, res) {
+  try {
+    res.render("login", { errors: [], data: [] });
+  } catch (err) {
+    console.error("ERROR with loginGet: ", err);
+    res.status(500).send("Server error");
+  }
+}
 
 // post login
 
 // get logout
 
-module.exports = { signUpGet };
+module.exports = { signUpGet, loginGet };
--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
 
 const authRouter = Router();
+const authController = require("../controllers/authController");
+
+authRouter.get("/signup", authController.signUpGet);
+
+authRouter.get("/login", authController.loginGet);
 
 module.exports = authRouter;
