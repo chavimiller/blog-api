ad24dcd4244ca0c1ef2313d10961aa49c1f5ac93
chavimiller <chaviweisman@gmail.com>
2026-01-01 15:48:17 +0200

    checkpoint before prisma reset

--- api/config/passport.js
+++ api/config/passport.js
@@ -10,9 +10,12 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
+
       return done(null, user);
     } catch (err) {
       return done(err);
