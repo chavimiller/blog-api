c8169dab92190fcf42e0314a65b0d9ba4005d94f
chavimiller <chaviweisman@gmail.com>
2026-01-01 16:50:21 +0200

    delete all prisma files to start from scratch

--- api/config/passport.js
+++ api/config/passport.js
@@ -39,3 +39,41 @@ passport.deserializeUser(async (id, done) => {
     done(err);
   }
 });
+
+/*
+
+model User {
+  id            Int     @id @default(autoincrement())
+  username      String  @unique
+  password_hash String
+  is_author     Boolean @default(false)
+
+  posts    Post[]    @relation("UserPosts")
+  comments Comment[] @relation("UserComments")
+}
+
+model Post {
+  id        Int     @id @default(autoincrement())
+  title     String
+  content   String?
+  published Boolean @default(false)
+
+  authorId Int
+  author   User @relation("UserPosts", fields: [authorId], references: [id], onDelete: Cascade)
+
+  comments Comment[]
+}
+
+model Comment {
+  id      Int    @id @default(autoincrement())
+  content String
+
+  postId Int
+  post   Post @relation(fields: [postId], references: [id], onDelete: Cascade)
+
+  authorId Int
+  author   User @relation("UserComments", fields: [authorId], references: [id], onDelete: Cascade)
+}
+
+
+*/
