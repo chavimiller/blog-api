01b886c4f022900aa2c25992601946a447914360
chavimiller <chaviweisman@gmail.com>
2026-01-04 10:05:56 +0200

    set up prisma session storage

--- api/app.js
+++ api/app.js
@@ -3,16 +3,34 @@ const app = express();
 const authRouter = require("./routes/authRouter");
 const postRouter = require("./routes/postRouter");
 const commentRouter = require("./routes/commentRouter");
+
 const path = require("node:path");
-const session = require("express-session");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+const prisma = require("./lib/prisma");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("Homepage"));
 
