29f46c9d9ec98857acc3384e824dba4bec0d021f
chavimiller <chaviweisman@gmail.com>
2026-01-12 12:59:38 +0200

    install react-router

--- author-client/src/App.jsx
+++ author-client/src/App.jsx
@@ -1,20 +1,12 @@
 import "./App.css";
 import Comment from "./components/Comment";
-import Home from "./components/Home";
-import SignUp from "./components/signUp";
-import Login from "./components/Login";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 import Post from "./components/Post";
 
 function App() {
-  return (
-    <>
-      <SignUp />
-      <Login />
-      <Home />
-      <Comment />
-      <Post />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
